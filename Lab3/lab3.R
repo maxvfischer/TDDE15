48fb9754aa1c07e10599ac2919b96627b80393eb
Max Fischer <maxfi539@student.liu.se>
2018-10-05 21:57:34 +0200

    Add the finished code to lab3

--- Lab3/lab3.R
+++ Lab3/lab3.R
@@ -2,8 +2,7 @@
 ###########   Lab 3   ############
 ##################################
 
-## Part 1
-
+#################### Functions ####################
 emission_model <- function(z_t, sd) {
   model <- sample(1:3, 1, replace = TRUE)
   
@@ -47,6 +46,7 @@ generate_data <- function(T, sd) {
   return (list("Z_true" = Z_true, "X_true" = X_true))
 }
 
+#################### Task 1 ####################
 ## Generate true hidden states and observations
 T <- 100 # Number of time steps
 sd <- 1
@@ -54,12 +54,13 @@ data <- generate_data(T, sd)
 
 ## Particle filter algorithm
 N <- 100 # Number of particles
-Z_particles_t_1 <- runif(n = N, min = 0, max = 100) # Initial particles (hypothesis of robot states)
-Z_particles_t <- rep(0, N) # Array that will be filled with new hypothesis
+Z_particles <- matrix(0, nrow = T, ncol = N) # Matrix with particles during each time step
+Z_particles[1, ] <- as.matrix(t(runif(n = N, min = 0, max = 100)))# Initial particles (hypothesis of robot states)
+Z_particles_temp <- rep(0, N) # Array that will be filled with new hypothesis
 weights <- rep(0, N)
 
 # For each time step
-for (t in 1:T) {
+for (t in 1:(T-1)) {
   
   # Generate N particles (hypothesis of hidden state)
   # via transition model p(z_t | z_t-1)
@@ -70,29 +71,41 @@ for (t in 1:T) {
   # p(z_t|z_t-1) = 1/3(N(z_t|µ=z_t, σ=1) + N(z_t|µ=z_t + 1, σ=1) + N(z_t|µ=z_t+2, σ=1))
   # Uniform to draw which distribution we're going to draw from
   for (n in 1:N) {
-    
+
     # Generate new particle (state hypothesis) from old one. p(z_t | z_t_1)
-    Z_particles_t[n] <- transition_model(Z_particles_t_1[n])
+    Z_particles_temp[n] <- transition_model(as.vector(Z_particles[t,n]))
     
     # Generate weight for the new hypothesis.
     # How likely is the observation in this time step, 
     # given the new hypothesis state (robot position)
-    weights[n] <- emission_density(data$X_true[t], Z_particles_t[n], 1)
+    weights[n] <- emission_density(data$X_true[t], Z_particles_temp[n], 1)
   }
   
   # Sample 100 particles (hypothesis), with respect to their
   # weight (relative "probability" among the particles)
-  Z_particles_t_1 <- sample(x = Z_particles_t,
+  Z_particles[t+1,] <- sample(x = Z_particles_temp,
                                size = 100,
                                prob = weights,
                                replace = TRUE)
 }
 
-hist(Z_particles_t_1)
-hist(data$Z_true)
+# First time step
+print(Z_particles[1, ]) # Particles
+print(data$Z_true[1]) # True location
 
-## Task 2
+# Last time step
+print(Z_particles[100, ]) # Particles
+print(data$Z_true[100]) # True location
 
+# 30 th time step
+print(Z_particles[30, ]) # Particles
+print(data$Z_true[30]) # True location
+
+# 60th time step
+print(Z_particles[60, ]) # Particles
+print(data$Z_true[60]) # True location
+
+#################### Task 2 ####################
 ## Generate true hidden states and observations
 T <- 100 # Number of time steps
 sigmas <- c(5, 50)
@@ -101,15 +114,19 @@ data_50 <- generate_data(T, sigmas[2])
 
 ## Particle filter algorithm
 N <- 100 # No of time steps
-Z_5_particles_t_1 <- runif(n = N, min = 0, max = 100) # Initial particles (hypothesis of robot states)
-Z_5_particles_t <- numeric(0) # Array that will be filled with new hypothesis
-Z_50_particles_t_1 <- runif(n = N, min = 0, max = 100)
-Z_50_particles_t <- rep(0, N)
+Z_5_particles <- matrix(0, nrow = T, ncol = N)
+Z_5_particles[1, ]<- runif(n = N, min = 0, max = 100) # Initial particles (hypothesis of robot states)
+Z_5_particles_temp <- rep(0, N) # Array that will be filled with new hypothesis
+
+Z_50_particles <- matrix(0, nrow = T, ncol = N)
+Z_50_particles[1, ] <- runif(n = N, min = 0, max = 100)
+Z_50_particles_temp <- rep(0, N)
+
 weights_5 <- rep(0, N)
 weights_50 <- rep(0, N)
 
 # For each time step
-for (t in 1:T) {
+for (t in 1:(T-1)) {
   
   # Generate N particles (hypothesis of hidden state)
   # via transition model p(z_t | z_t-1)
@@ -122,32 +139,113 @@ for (t in 1:T) {
   for (n in 1:N) {
     
     # Generate new particle (state hypothesis) from old one. p(z_t | z_t_1)
-    Z_5_particles_t[n] <- transition_model(Z_5_particles_t_1[n])
-    Z_50_particles_t[n] <- transition_model(Z_50_particles_t_1[n])
+    Z_5_particles_temp[n] <- transition_model(as.vector(Z_5_particles[t, n]))
+    Z_50_particles_temp[n] <- transition_model(as.vector(Z_50_particles[t, n]))
     
     # Generate weight for the new hypothesis.
     # How likely is the observation in this time step, 
     # given the new hypothesis state (robot position)
-    weights_5[n] <- emission_density(data_5$X_true[n], Z_5_particles_t[n], sigmas[1])
-    weights_50[n] <- emission_density(data_50$X_true[n], Z_50_particles_t[n], sigmas[2])
+    weights_5[n] <- emission_density(data_5$X_true[n], Z_5_particles_temp[n], sigmas[1])
+    weights_50[n] <- emission_density(data_50$X_true[n], Z_50_particles_temp[n], sigmas[2])
   }
   
   # Sample 100 particles (hypothesis), with respect to their
   # weight (relative "probability" among the particles)
-  Z_5_particles_t_1 <- sample(x = Z_5_particles_t,
+  Z_5_particles[t+1, ] <- sample(x = Z_5_particles_temp,
                             size = 100,
                             prob = weights_5,
                             replace = TRUE)
-  Z_50_particles_t_1 <- sample(x = Z_50_particles_t,
+  
+  Z_50_particles[t+1, ] <- sample(x = Z_50_particles_temp,
                             size = 100,
                             prob = weights_50,
                             replace = TRUE)
 }
 
-par(mfrow=c(2,2))
-hist(Z_5_particles_t_1)
-hist(data_5$Z_true)
-hist(Z_50_particles_t_1)
-hist(data_50$Z_true)
+## Sigma = 5
+# First time step
+print(Z_5_particles[1, ]) # Particles
+print(data_5$Z_true[1]) # True location
 
-## Part 3
+# Last time step
+print(Z_5_particles[100, ]) # Particles
+print(data_5$Z_true[100]) # True location
+
+# 30 th time step
+print(Z_5_particles[30, ]) # Particles
+print(data_5$Z_true[30]) # True location
+
+# 60th time step
+print(Z_5_particles[60, ]) # Particles
+print(data_5$Z_true[60 ]) # True location
+
+## Sigma = 50
+# First time step
+print(Z_50_particles[1, ]) # Particles
+print(data_50$Z_true[1]) # True location
+
+# Last time step
+print(Z_50_particles[100, ]) # Particles
+print(data_50$Z_true[100]) # True location
+
+# 30 th time step
+print(Z_50_particles[30, ]) # Particles
+print(data_50$Z_true[30]) # True location
+
+# 60th time step
+print(Z_50_particles[60, ]) # Particles
+print(data_50$Z_true[60 ]) # True location
+
+#################### Task 3 ####################
+## Generate true hidden states and observations
+T <- 100 # Number of time steps
+sd <- 1
+data <- generate_data(T, sd)
+
+## Particle filter algorithm
+N <- 100 # Number of particles
+Z_particles <- matrix(0, nrow = T, ncol = N)
+Z_particles[1, ] <- runif(n = N, min = 0, max = 100) # Initial particles (hypothesis of robot states)
+Z_particles_temp <- rep(0, N) # Array that will be filled with new hypothesis
+weights <- rep(1, N)
+
+# For each time step
+for (t in 1:(T-1)) {
+  
+  # Generate N particles (hypothesis of hidden state)
+  # via transition model p(z_t | z_t-1)
+  # Given that we are here (z_t-1), this is the probabilities of
+  # where we will be in the next time step
+  # 
+  # We use a mixed transition model:
+  # p(z_t|z_t-1) = 1/3(N(z_t|µ=z_t, σ=1) + N(z_t|µ=z_t + 1, σ=1) + N(z_t|µ=z_t+2, σ=1))
+  # Uniform to draw which distribution we're going to draw from
+  for (n in 1:N) {
+    
+    # Generate new particle (state hypothesis) from old one. p(z_t | z_t_1)
+    Z_particles_temp[n] <- transition_model(as.vector(Z_particles[t, n]))
+  }
+  
+  # Sample 100 particles (hypothesis), with respect to their
+  # weight (relative "probability" among the particles)
+  Z_particles[t+1, ] <- sample(x = Z_particles_temp,
+                              size = 100,
+                              prob = weights,
+                              replace = TRUE)
+}
+
+# First time step
+print(Z_particles[1, ]) # Particles
+print(data$Z_true[1]) # True location
+
+# Last time step
+print(Z_particles[100, ]) # Particles
+print(data$Z_true[100]) # True location
+
+# 30 th time step
+print(Z_particles[30, ]) # Particles
+print(data$Z_true[30]) # True location
+
+# 60th time step
+print(Z_particles[60, ]) # Particles
+print(data$Z_true[60]) # True location
