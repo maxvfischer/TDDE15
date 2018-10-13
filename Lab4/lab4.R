b5351256d0c688bc7598dae1be6f486f739dc395
Max Fischer <maxfi539@student.liu.se>
2018-10-13 08:01:08 +0200

    Add task 2, sub tasks 1-5

--- Lab4/lab4.R
+++ Lab4/lab4.R
@@ -13,8 +13,34 @@ squared_exponential <- function(X, X_tick, sigma, l) {
   return(K)
 }
 
+plotGP <- function(x, y, x_star, GP_mean, GP_var) {
+  
+  # Calculate lower and upper 95 % pointwise confidence interval
+  GP_CI <- data.frame(upper = as.vector(GP_mean) + 1.96 * sqrt(GP_var),
+                    lower = as.vector(GP_mean) - 1.96 * sqrt(GP_var))
+  
+  # Upper and lower y-limits based on max/min mean +- std (pointwise)
+  y_lim <- c(max(GP_CI$upper) + 0.5, min(GP_CI$lower) - 0.5)
+  
+  plot(x = x_star, 
+       y = GP_mean, 
+       type = 'l', 
+       ylim = y_lim,
+       xlab = 'x-values',
+       ylab = 'Posterior probabilities')
+  polygon(c(rev(x_star), x_star), 
+          c(rev(GP_CI$upper), GP_CI$lower), 
+          col = 'grey80', border = NA,
+          ylim = y_lim,
+          xlab = 'x-values',
+          ylab = 'Posterior probabilities')
+  lines(x = x_star, y = GP_mean)
+  lines(x = x_star, y = GP_CI$upper, lty = 'dashed', col = 'red')
+  lines(x = x_star, y = GP_CI$lower, lty = 'dashed', col = 'red')
+  points(x = x, y = y, type = 'p')
+}
 
-#################### Task 1 ####################
+#################### Task 2.2.1 ####################
 ## Input:
 # x: Vector of training inputs.
 # y: Vector of training targets/outputs.
@@ -43,7 +69,7 @@ posteriorGP <- function(x, y, XStar,
 
   # Cholesky factorization
   # (K + sigma_identity) = L %*% t(L)
-  L_upper <- chol(x = (K.X_X + diagonal_variance_noise))
+  L_upper <- chol(K.X_X + diagonal_variance_noise)
   L_lower <- t(L_upper)
 
   # Predictive mean
@@ -55,11 +81,11 @@ posteriorGP <- function(x, y, XStar,
   
   # Predictive variance
   v <- solve(a = L_lower, b = K.X_XStar)
-  f_variance <- K.XStar_XStar - t(v) %*% v
+  f_variance <- diag(K.XStar_XStar - t(v) %*% v)
   return (list('pred_mean' = f_mean, 'pred_var' = f_variance))
 }
 
-#################### Task 2 ####################
+#################### Task 2.2.2 ####################
 sigma_f <- 1
 l <- 0.3
 hyperParam <- c(sigma_f, l)
@@ -72,15 +98,62 @@ GP_posterior <- posteriorGP(x = x, y = y, XStar = x_star,
                              hyperParam = hyperParam,
                              sigmaNoise = sigma_n)
 
-# Calculate lower and upper 95 % pointwise confidence interval
-GP_CI <- data.frame(upper = as.vector(GP_posterior$pred_mean) + 1.96 * as.vector(sqrt(GP_posterior$pred_var)),
-                    lower = as.vector(GP_posterior$pred_mean) - 1.96 * as.vector(sqrt(GP_posterior$pred_var)))
-plot(x = x_star, y = GP_posterior$pred_mean, type = 'l')
-lines(x = x_star, y = GP_CI$upper)
-lines(x = x_star, y = GP_CI$lower)
-######################
+GP_CI <- data.frame(upper = as.vector(GP_posterior$pred_mean) + 1.96 * sqrt(GP_posterior$pred_var),
+                    lower = as.vector(GP_posterior$pred_mean) - 1.96 * sqrt(GP_posterior$pred_var))
+plotGP(x = x, y = y, x_star = x_star, 
+       GP_mean = GP_posterior$pred_mean,
+       GP_var = GP_posterior$pred_var)
 
+#################### Task 2.2.3 ####################
+sigma_f <- 1
+l <- 0.3
+hyperParam <- c(sigma_f, l)
+x <- c(0.4, -0.6)
+y <- c(0.719, -0.044)
 
+x_star <- seq(from = -1, to = 1, by = 0.01)
+sigma_n <- 0.1
 
+GP_posterior <- posteriorGP(x = x, y = y, XStar = x_star,
+                            hyperParam = hyperParam,
+                            sigmaNoise = sigma_n)
 
+plotGP(x = x, y = y, x_star = x_star,
+       GP_mean = GP_posterior$pred_mean,
+       GP_var = GP_posterior$pred_var)
 
+#################### Task 2.2.4 ####################
+sigma_f <- 1
+l <- 0.3
+hyperParam <- c(sigma_f, l)
+x <- c(-1.0, -0.6, -0.2, 0.4, 0.8)
+y <- c(0.768, -0.044, -0.940, 0.719, -0.664)
+
+x_star <- seq(-1, 1, 0.01)
+sigma_n <- 0.1
+
+GP_posterior <- posteriorGP(x = x, y = y, XStar = x_star,
+                            hyperParam = hyperParam,
+                            sigmaNoise = sigma_n)
+
+plotGP(x = x, y = y, x_star = x_star,
+       GP_mean = GP_posterior$pred_mean,
+       GP_var = GP_posterior$pred_var)
+
+#################### Task 2.2.5 ####################
+sigma_f <- 1
+l <- 1
+hyperParam <- c(sigma_f, l)
+x <- c(-1.0, -0.6, -0.2, 0.4, 0.8)
+y <- c(0.768, -0.044, -0.940, 0.719, -0.664)
+
+x_star <- seq(-1, 1, 0.01)
+sigma_n <- 0.1
+
+GP_posterior <- posteriorGP(x = x, y = y, XStar = x_star,
+                            hyperParam = hyperParam,
+                            sigmaNoise = sigma_n)
+
+plotGP(x = x, y = y, x_star = x_star,
+       GP_mean = GP_posterior$pred_mean,
+       GP_var = GP_posterior$pred_var)
